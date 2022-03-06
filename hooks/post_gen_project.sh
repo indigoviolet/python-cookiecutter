058e1882af27efd5a8ab35477fff4ca760f0edc3
Venky Iyer <indigoviolet@gmail.com>
2022-03-06 10:17:14 -0800

    add remote

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -7,7 +7,7 @@ direnv allow
 git init
 
 {% if cookiecutter.push_to_github != "no" %}
-gh repo create {{cookiecutter.project_name}} -d "{{cookiecutter.description}}" --{{cookiecutter.push_to_github}}
+gh repo create {{cookiecutter.project_name}} -d "{{cookiecutter.description}}" --{{cookiecutter.push_to_github}} --source=. --remote=upstream --disable-wiki --push
 {% endif %}
 
 poetry add -D mypy
