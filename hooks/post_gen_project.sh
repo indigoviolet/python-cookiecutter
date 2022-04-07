abae0f02b5531f1aa4fb66dfdcd7dc5b8a994bf9
Venky Iyer <indigoviolet@gmail.com>
2022-04-07 10:27:41 -0700

    remove --push

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -7,7 +7,7 @@ direnv allow
 git init
 
 {% if cookiecutter.push_to_github != "no" %}
-gh repo create {{cookiecutter.project_name}} -d "{{cookiecutter.description}}" --{{cookiecutter.push_to_github}} --source=. --remote=upstream --disable-wiki --push
+gh repo create {{cookiecutter.project_name}} -d "{{cookiecutter.description}}" --{{cookiecutter.push_to_github}} --source=. --remote=upstream --disable-wiki
 {% endif %}
 
 poetry add -D mypy
